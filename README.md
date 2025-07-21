f3b69e890f894bd7923a6ec90563d87ef423dddb
reallygoodprogrammer <milesdustin45@gmail.com>
2025-07-20 17:46:12 -0700

    first commit yay

---
+++ README.md
@@ -0,0 +1,10 @@
+# iof-tree
+
+usage: `iof-tree [-c concurrency-level] url [urls]+`
+
+A go script for recursing through an 'Index of' directory page, only
+pulling urls from other pages with 'Index of' prefix in title. This 
+tool is probably not as intensive as it could be currently; there
+are probably versions of these directory pages that don't match the 
+same format that I am matching for in this script. I'll likely add
+more to it as I need it.
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"sync"
+	"sync/atomic"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	concurrency := flag.Int("c", 10, "concurrency level")
+	flag.Parse()
+
+	urls := flag.Args()
+
+	input := make(chan string)
+	output := make(chan string)
+	err_output := make(chan error)
+	var count int64 = 0
+
+	diminish := func() {
+		if atomic.AddInt64(&count, -1) == 0 {
+			close(input)
+		}
+	}
+
+	passes := func(val string) bool {
+		return val != "/" && !strings.HasPrefix(val, "?") &&
+		!strings.HasPrefix(val, "http://") && !strings.HasPrefix(val, "https://") &&
+		!strings.HasPrefix(val, "/")
+	}
+
+	var process func(u string)
+	process = func(u string) {
+		defer diminish()
+
+		resp, err := http.Get(u)
+		if err != nil {
+			err_output <- err
+			return
+		}
+		defer resp.Body.Close()
+
+		contentType := resp.Header.Get("Content-Type")
+		if !strings.HasPrefix(contentType, "text/html") {
+			return
+		}
+
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			err_output <- err
+			return
+		}
+
+		if !strings.Contains(doc.Find("title").Text(), "Index of") {
+			return
+		}
+
+		doc.Find("a[href]").Each(func(i int, s *goquery.Selection) {
+			val, valid := s.Attr("href")
+			if valid && passes(val) {
+				if strings.HasSuffix(val, "/") {
+					atomic.AddInt64(&count, 1)
+					select {
+					case input <- u + val:
+						// swagged up
+					default:
+						process(u + val)
+					}
+				}
+				output <- u + val
+			}
+		})
+	}
+
+	var inwg sync.WaitGroup
+	for i := 0; i < *concurrency; i++ {
+		inwg.Add(1)
+		go func() {
+			defer inwg.Done()
+			for u := range input {
+				process(u)
+			}
+		}()
+	}
+
+	var outwg sync.WaitGroup
+	outwg.Add(1)
+	go func() {
+		defer outwg.Done()
+		for o := range output {
+			fmt.Println(o)
+		}
+	}()
+
+	var errwg sync.WaitGroup
+	errwg.Add(1)
+	go func() {
+		defer errwg.Done()
+		for e := range err_output {
+			fmt.Fprintln(os.Stderr, e)
+		}
+	}()
+
+	if len(urls) == 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			atomic.AddInt64(&count, 1)
+			input <- scanner.Text()
+		}
+	} else {
+		for _, u := range urls {
+			atomic.AddInt64(&count, 1)
+			input <- u
+		}
+	}
+
+	inwg.Wait()
+	close(output)
+	close(err_output)
+	outwg.Wait()
+	errwg.Wait()
+}
